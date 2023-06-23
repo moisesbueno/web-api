505f6dac12f56ae3a7a3d18ec7c88622f715ec5f
Moisés Bueno <moisesbueno@ioasys.com.br>
2023-06-23 11:27:35 -0300

    adiciona projeto Client

---
+++ Client/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using var client = new HttpClient();
+
+var result = await client.GetStringAsync("http://localhost:5000/api/book");
+
+
+Console.WriteLine(result);
+Console.ReadKey();
